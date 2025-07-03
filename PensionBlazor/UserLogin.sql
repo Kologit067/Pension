6fbfa2fb38fc2e72b73c10d758dde89068a833f8
Kolomiyets Oleg <kologit.067@gmail.com>
2025-07-03 14:39:33 +0300

    add UserLogin sql

---
+++ PensionBlazor/UserLogin.sql
@@ -0,0 +1,10 @@
+﻿USE [Pension]
+GO
+SET IDENTITY_INSERT [dbo].[UserLogin] ON 
+GO
+INSERT [dbo].[UserLogin] ([UserLoginId], [email], [password], [token]) VALUES (1, N'kolo.067@gmail.com', N'', N'')
+GO
+INSERT [dbo].[UserLogin] ([UserLoginId], [email], [password], [token]) VALUES (2, N'kolo.git.067@gmail.com', N'111', N'')
+GO
+SET IDENTITY_INSERT [dbo].[UserLogin] OFF
+GO
